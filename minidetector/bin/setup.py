c5b64f31bc4953e35b243eef0aaaeaeac1c3d378
Ido <ido@pop-os.localdomain>
2020-12-22 01:51:40 +0200

    Removed requirement for README in the minidetector service. Added the minidetecotr.env for global config to the stack

--- minidetector/bin/setup.py
+++ minidetector/bin/setup.py
@@ -1,16 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name="minidetector",
     version="0.0.1",
     author="Amit Itzkovitch",
     author_email="amit7itz@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    description="A small example package",  
     packages=setuptools.find_packages(),
     install_requires=['scapy', 'sqlalchemy', 'psycopg2'],
     classifiers=[
